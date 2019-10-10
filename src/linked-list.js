821cf2441445d76cd6bc9c637951645cef702aec
BeaRRRRR <mishanx873@gmail.com>
2019-10-10 14:42:02 +0300

    feat : solve the task

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,131 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this.length = 0;
+        this._head;
+        this._tail;
+    }
 
-    append(data) {}
+    append(data) {
+        const newNode = new Node(data);
 
-    head() {}
+        // If there is no head yet let's make new node a head.
+        if (!this._head) {
+            this._head = newNode;
+            this._tail = newNode;
+            this.length++;
 
-    tail() {}
+            return this;
+        }
 
-    at(index) {}
+        // Attach new node to the end of linked list.
+        this._tail.next = newNode;
 
-    insertAt(index, data) {}
+        // Attach current tail to the new node's previous reference.
+        newNode.prev = this._tail;
 
-    isEmpty() {}
+        // Set new node to be the tail of linked list.
+        this._tail = newNode;
+        this.length++;
 
-    clear() {}
+        return this;
+    }
 
-    deleteAt(index) {}
+    head() {
+        return this._head.data;
+    }
 
-    reverse() {}
+    tail() {
+        return this._tail.data;
+    }
 
-    indexOf(data) {}
+    at(index) {
+        let node = this._head;
+        for (let i = 0; i < index; i++) {
+            node = node.next;
+        }
+        return node.data;
+    }
+
+    insertAt(index, data) {
+        if(this.length) {
+            let node = this._head;
+            for (let i = 0; i < index; i++) {
+                node = node.next;
+            }
+            let nodeToInsert = new Node(data, node.prev, node);
+            let _prev = node.prev;
+            node.prev = nodeToInsert;
+            _prev.next = nodeToInsert;
+            this.length++;
+        }
+        else this.append(data);
+    }
+
+    isEmpty() {
+        if (this.length == 0) return true;
+        return false;
+    }
+
+    clear() {
+        this._tail = new Node();
+        this._head = this._tail;
+        this.length = 0;
+        return this;
+    }
+
+    deleteAt(index) {
+        let node = this._head;
+        for (let i = 0; i < index; i++) {
+            node = node.next;
+        }
+        if(index != 0) {
+            node.next.prev = node.prev;
+            node.prev.next = node.next;
+        }
+        this.length--;
+
+        return this;
+    }
+
+    reverse() {
+        let currNode = this._head;
+        let prevNode = null;
+        let nextNode = null;
+
+        while (currNode) {
+            // Store next node.
+            nextNode = currNode.next;
+            prevNode = currNode.prev;
+
+            // Change next node of the current node so it would link to previous node.
+            currNode.next = prevNode;
+            currNode.prev = nextNode;
+
+            // Move prevNode and currNode nodes one step forward.
+            prevNode = currNode;
+            currNode = nextNode;
+        }
+
+        // Reset head and tail.
+        this._tail = this._head;
+        this._head = prevNode;
+
+        return this;
+
+    }
+
+    indexOf(data) {
+        let node = this._head;
+        if (node.data == data) return 0;
+        for (let i = 1; i < this.length; i++) {
+            node = node.next;
+            if (node.data == data) return i;
+        }
+        return -1;
+    }
 }
+
 
 module.exports = LinkedList;
